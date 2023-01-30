c9bc2c13707b96c3d0c40ea700b27098c9f1aa37
Mahamadi NIKIEMA <nikiemamahamadi01@gmail.com>
2023-01-30 16:00:13 +0100

    add Ligth-FM algo

--- cf-modeling.py
+++ cf-modeling.py
@@ -1,31 +1,25 @@
 import pandas as pd
-from surprise import Dataset,Reader
-from surprise.model_selection import cross_validate
-from surprise import KNNBasic,KNNWithMeans,accuracy
-from surprise.model_selection import train_test_split
+from lightfm import LightFM
+from scipy.sparse import csr_matrix
 
 
 
 
+df = pd.read_csv("data/goodreads_interactions.csv", sep=",", nrows=100_000)
+df = df[["user_id","book_id", "rating"]]
 
-df = pd.read_csv("data/goodreads_interactions.csv", sep=",", nrows=10_000)
-df = df[["user_id","book_id", "rating"]].rename(columns={"user_id":"userID",
-                                    "book_id":"itemID","rating":"rating"})
-                                    
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df, reader)
+# fil na with 0 after pivoting
+user_mat = df.pivot_table(index="user_id",columns="book_id", values="rating").fillna(0)
 
-trainset, testset=train_test_split(data, test_size=0.2)
-#item-based (Cosine)
-sim_options = {
-    "user_based": False,  # compute  similarities between users
-    "name":"cosine"
-}
-algo = KNNBasic(sim_options=sim_options,k=40, min_k=5) #default value of k is 40
-# 
-#cv=cross_validate(algo, data, measures=["RMSE", "MAE"], cv=5, return_train_measures=True, verbose=True)
-algo.fit(trainset)
-predictions = algo.test(testset)
-accuracy.rmse(predictions)
-accuracy.mae(predictions)
+# use scipy csr matrix
+user_mat_csr = csr_matrix(user_mat.values)
 
+# Use LightFM
+model = LightFM(loss='warp',
+                random_state=2016,
+                learning_rate=0.90,
+                no_components=150,
+                user_alpha=0.000005)
+model = model.fit(user_mat_csr,
+                  epochs=100,
+                  num_threads=16, verbose=False)
