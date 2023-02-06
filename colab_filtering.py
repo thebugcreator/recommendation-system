81ebed5e4aa3469745817def98cca5d01a50ca05
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-06 14:49:12 +0100

    Added spark instantiation

--- colab_filtering.py
+++ colab_filtering.py
@@ -2,6 +2,14 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark RegSys example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
 ratings = spark.read.option("header", True).csv("datasets/goodreads_interactions_sample.csv")
 (training, test) = ratings.randomSplit([0.8, 0.2])
 
