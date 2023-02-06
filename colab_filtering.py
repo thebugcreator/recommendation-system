c42425e5f67d9475ff702b397f68c2c2e9b19c2f
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-06 14:45:07 +0100

    Complete the colfilt

--- colab_filtering.py
+++ colab_filtering.py
@@ -1,16 +1,33 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark RecSys") \
-    .config("spark.some.config.option", "some-value") \
-    .getOrCreate()
-
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
-ratings = spark.read.option("header", True).csv("datasets/goodreads_interactions_sample.csv")
+lines = spark.read.csv("datasets/goodreads_interactions_sample.csv").rdd
+parts = lines.map(lambda row: row.value.split("::"))
+ratingsRDD = parts.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]),
+                                     rating=float(p[2]), timestamp=int(p[3])))
+ratings = spark.createDataFrame(ratingsRDD)
 (training, test) = ratings.randomSplit([0.8, 0.2])
 
+als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="book_id", ratingCol="rating",
+          coldStartStrategy="drop")
+model = als.fit(training)
 
+# Evaluate the model by computing the RMSE on the test data
+predictions = model.transform(test)
+evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
+                                predictionCol="prediction")
+rmse = evaluator.evaluate(predictions)
+print("Root-mean-square error = " + str(rmse))
+
+# Generate top 10 movie recommendations for each user
+userRecs = model.recommendForAllUsers(10)
+# Generate top 10 user recommendations for each book
+bookRecs = model.recommendForAllItems(10)
+
+# Generate top 10 movie recommendations for a specified set of users
+users = ratings.select(als.getUserCol()).distinct().limit(3)
+userSubsetRecs = model.recommendForUserSubset(users, 10)
+# Generate top 10 user recommendations for a specified set of books
+books = ratings.select(als.getItemCol()).distinct().limit(3)
+booksSubSetRecs = model.recommendForItemSubset(books, 10)
