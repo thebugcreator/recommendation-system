38dfbd926c32b44bdec8031f72bcaad254a692db
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-06 14:46:36 +0100

    Refine the colfilt

--- colab_filtering.py
+++ colab_filtering.py
@@ -2,11 +2,7 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
-lines = spark.read.csv("datasets/goodreads_interactions_sample.csv").rdd
-parts = lines.map(lambda row: row.value.split("::"))
-ratingsRDD = parts.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]),
-                                     rating=float(p[2]), timestamp=int(p[3])))
-ratings = spark.createDataFrame(ratingsRDD)
+ratings = spark.read.option("header", True).csv("datasets/goodreads_interactions_sample.csv")
 (training, test) = ratings.randomSplit([0.8, 0.2])
 
 als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="book_id", ratingCol="rating",
