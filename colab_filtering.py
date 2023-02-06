6c7c2ac743b6c3705bda85c04fd34a7af7fc0cb3
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-06 14:41:26 +0100

    Cleaned the fussy code

--- colab_filtering.py
+++ colab_filtering.py
@@ -1,10 +1,16 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark RecSys") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import Row
 
-lines = spark.read.csv("datasets/goodreads_interactions_sample.csv").rdd
-parts = lines.map(lambda row: row.value.split("::"))
-ratingsRDD = parts.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]),
-                                     rating=float(p[2]), timestamp=int(p[3])))
-ratings = spark.createDataFrame(ratingsRDD)
+ratings = spark.read.option("header", True).csv("datasets/goodreads_interactions_sample.csv")
 (training, test) = ratings.randomSplit([0.8, 0.2])
+
+
