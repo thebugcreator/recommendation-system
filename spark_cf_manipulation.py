37569b8c3e4bb724935d1118215e33ceb19c953f
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-10 10:45:12 +0100

    Initialised the manipulation for CF

--- spark_cf_manipulation.py
+++ spark_cf_manipulation.py
@@ -1,3 +1,42 @@
 from pyspark.sql import Row
 from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS
 
+
+# Create a spark session
+xmx = "16g"
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark RegSys example") \
+    .config("spark.driver.memory", xmx) \
+    .getOrCreate()
+
+# TODO: Load the spark ALS model
+model = ALS.load("als_models")
+# TODO: Lazy loading the related dataframes
+df_users = spark.read.option("header", True).option("inferSchema", True).csv("datasets/goodreads_users.csv")
+df_books = spark.read.option("header", True).option("inferSchema", True).csv("datasets/goodreads_books.csv")
+
+
+def recommend_books_for_each_user(n=10):
+    """Generate top 10 movie recommendations for each user"""
+    userRecs = model.recommendForAllUsers(n)
+    return userRecs
+
+def recommend_users_for_each_book(n=10):
+    """Generate top 10 user recommendations for each book"""
+    bookRecs = model.recommendForAllItems(10)
+    return bookRecs
+    
+
+def recommend_books_for_specific_users(user_ids, n=10):
+    """Generate top 10 movie recommendations for a specified set of users"""
+    users = ratings.select(als.getUserCol()).distinct().limit(3)
+    userSubsetRecs = model.recommendForUserSubset(users, n)
+    return userSubsetRecs
+
+def recommend_users_for_specific_books(book_ids, n=10)
+    """Generate top 10 user recommendations for a specified set of books"""
+    books = ratings.select(als.getItemCol()).distinct().limit(3)
+    booksSubSetRecs = model.recommendForItemSubset(books, n)
+    return booksSubSetRecs
