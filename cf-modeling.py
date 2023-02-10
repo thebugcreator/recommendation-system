732d3e6a24b072a6eddae589fdf1a749a174b922
Mahamadi NIKIEMA <nikiemamahamadi01@gmail.com>
2023-02-10 13:44:25 +0100

    refactor codes

--- cf-modeling.py
+++ cf-modeling.py
@@ -1,42 +1,33 @@
 import pandas as pd
 from lightfm import LightFM
 from scipy import sparse
-from lightfm.cross_validation import random_train_test_split
 from lightfm.evaluation import auc_score
 from pandas.api.types import CategoricalDtype
 import pickle
 import json
 import numpy as np
 
-#import the dataset
-df_csv = pd.read_csv("./data/data_sample.csv")
+def data_csr(df_csv):
 
-# Drop out books with less than 6 interactions
+    # build the scipy csr matrix
+    users = df_csv["user_id"].unique()
+    movies = df_csv["book_id"].unique()
+    shape = (len(users), len(movies))
+    user_cat = CategoricalDtype(categories=sorted(users), ordered=True)
+    movie_cat = CategoricalDtype(categories=sorted(movies), ordered=True)
+    user_index = df_csv["user_id"].astype(user_cat).cat.codes
+    movie_index = df_csv["book_id"].astype(movie_cat).cat.codes
+    coo = sparse.coo_matrix((df_csv["rating"], (user_index, movie_index)), shape=shape)
+    user_mat = coo.tocsr()
 
-book_filter = (df_csv["book_id"].value_counts()>5).to_dict()
-book_filter = df_csv["book_id"].map(book_filter)
-df_csv = df_csv.loc[book_filter,:]
+    print(f"Number of book used: {shape[0]}")
+    print(f"Number of users: {shape[1]}")
 
-print("sparsity is ",
-                    100-(df_csv.shape[0]/(df_csv['book_id'].max()*df_csv['user_id'].max())*100))
+    return user_mat
 
-# build the scipy csr matrix
-users = df_csv["user_id"].unique()
-movies = df_csv["book_id"].unique()
-shape = (len(users), len(movies))
-user_cat = CategoricalDtype(categories=sorted(users), ordered=True)
-movie_cat = CategoricalDtype(categories=sorted(movies), ordered=True)
-user_index = df_csv["user_id"].astype(user_cat).cat.codes
-movie_index = df_csv["book_id"].astype(movie_cat).cat.codes
-coo = sparse.coo_matrix((df_csv["rating"], (user_index, movie_index)), shape=shape)
-user_mat = coo.tocsr()
 
-print(f"Number of book used: {shape[0]}")
-print(f"Number of users: {shape[1]}")
 
 # Split data in train/test
-
-x_train,x_test = random_train_test_split(user_mat)
 
 # Modelling LightFM
 
@@ -47,12 +38,21 @@ model = LightFM(loss='warp',
                 learning_schedule="adadelta",
                 user_alpha=0.0005)
 
-model = model.fit(x_train,
-                  epochs=10,
-                  num_threads=16, verbose=False)
+def get_model(model, x_train =None, train=False):
 
-with open('model.pickle', 'wb') as fle:
-    pickle.dump(model, fle, protocol=pickle.HIGHEST_PROTOCOL)
+    if train:
+
+        model = model.fit(x_train,
+                        epochs=10,
+                        num_threads=16, verbose=False)
+        print("Model trained and save at ./lightfm-model/")
+        with open('model.pickle', 'wb') as fle:
+            pickle.dump(model, fle, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        model = pickle.loads("./lightfm-model/model.pickle")
+    return model
+
+
 
 auc = auc_score(model, x_test, num_threads=4).mean()
 print(f"AUC is :  {auc}")
--- recommend.py
+++ recommend.py
@@ -1,5 +1,17 @@
 import pickle
 import json
 import pandas as pd
+from lightfm.cross_validation import random_train_test_split
 
+
+#import the dataset
 df_csv = pd.read_csv("./data/data_sample.csv")
+
+# Drop out books with less than 6 interactions
+
+book_filter = (df_csv["book_id"].value_counts()>5).to_dict()
+book_filter = df_csv["book_id"].map(book_filter)
+df_csv = df_csv.loc[book_filter,:]
+print("sparsity is ",
+                    100-(df_csv.shape[0]/(df_csv['book_id'].max()*df_csv['user_id'].max())*100))
+x_train,x_test = random_train_test_split(user_mat)
