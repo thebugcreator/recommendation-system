c45da313e8d0148b1f8324488cd264678db7f177
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-13 23:47:47 +0100

    Minor mistakes again

--- implicit_als_manipulation.py
+++ implicit_als_manipulation.py
@@ -7,21 +7,21 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train the Colaborative Filtering Model with Implicit")
-    parser.add_argument("--model_path", type=str, default="implicit_als_model/", help="Path to the model folder.")
+    parser.add_argument("--model_folder", type=str, default="implicit_als_model/", help="Path to the model folder.")
     parser.add_argument("--type", default="user", const="all", nargs="?", help="Choose the subject for recommendation", choices=["user", "book"])
     parser.add_argument("query", type=int, help="ID of the subject.")
     args = parser.parse_args()
     
-    model_path = args.model_path
+    model_folder = args.model_folder
     type = args.type
     query = args.query
 
 
-    model_path = model_path + "model.npz"
+    model_path = model_folder + "model.npz"
 
     model = implicit.als.AlternatingLeastSquares(model_path)
-    sparse_book_user = sparse.load_npz(model_path + "book_user.npz")
-    sparse_user_book = sparse.load_npz(model_path + "user_book.npz")
+    sparse_book_user = sparse.load_npz(model_folder + "book_user.npz")
+    sparse_user_book = sparse.load_npz(model_folder + "user_book.npz")
     
     if type == "user":
         recommendation = model.recommend(query, sparse_user_book[query])
