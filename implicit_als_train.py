f59fd3099c410182c636b8a6c5b4bf38d596a27c
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2023-02-13 14:27:54 +0100

    Finalised x2 the ALS model

--- implicit_als_train.py
+++ implicit_als_train.py
@@ -18,19 +18,29 @@ def calc_confidence(is_read, rating, is_reviewed, weights=(1,1,1)):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train the Colaborative Filtering Model with Implicit")
     parser.add_argument("--csv_path", type=str, default="datasets/goodreads_interactions.csv", help="Path to the csv interaction file.")
-    parser.add_argument("--save_model", type=bool, default=False, help="Decide whether or not to save the model")
-    parser.add_argument("--metric", type=str, default="rmse", help="Evaluation metrics")
+    parser.add_argument("--save_model", type=bool, default=True, help="Decide whether or not to save the model")
+    parser.add_argument("--factors", type=int, default=20, help="Model factors")
+    parser.add_argument("--regularization", type=float, default=0.1, help="Model regularisation")
+    parser.add_argument("--iterations", type=int, default=20, help="Model iteration")
+    parser.add_argument("--alpha", type=int, default=40, help="Model alpha")
     args = parser.parse_args()
     
     csv_path = args.csv_path
     save_model = args.save_model
     metric = args.metric
+    factors = args.factors
+    regularization = args.regularization
+    iterations = args.iterations
+    alpha = args.alpha
     
+    print("Looking into the dataset at ", csv_path)
     # Load the interaction dataset
     df = pd.read_csv(csv_path)
     
     # Start creating the confidence matrix
     # df["confidence"] = [calc_confidence(*(row[["is_read","rating","is_reviewed"]].values)) for index, row in df.iterrows()]
+    
+    print("Number of records: ", df.shape[0])
     
     data = df[["user_id", "book_id", "rating"]]
     
@@ -38,8 +48,13 @@ if __name__ == "__main__":
     sparse_user_item = sparse.csr_matrix((data['rating'].astype(float), (data['user_id'], data['book_id'])))
     
     # Building the model
-    model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=20)
-    alpha_val = 40
+    model = implicit.als.AlternatingLeastSquares(factors=factors, regularization=regularization, iterations=iterations)
+    alpha_val = alpha
     data_conf = (sparse_item_user * alpha_val).astype('double')
+    
+    print("Start training the model")
     model.fit(data_conf)
-
+    print("Finish training the model")
+    if save_model:
+        model.save("implicit_als_model/model")
+        print("Saved the model")
